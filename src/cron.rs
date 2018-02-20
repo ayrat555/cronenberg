6a719acc069694661ed311382d77627b68b3048f
Ayrat Badykov <ayratin555@gmail.com>
2018-02-20 14:44:02 +0300

    parse to struct

--- src/cron.rs
+++ src/cron.rs
@@ -17,6 +17,7 @@ fn is_time_char(c: char) -> bool {
 }
 
 fn to_time_item(c: &str) -> Result<Option<u8>, ParseIntError> {
+    println!("{}", c);
     if c == "*" { return Ok(None) }
 
     match u8::from_str_radix(c, 10) {
@@ -31,24 +32,28 @@ named!(time_items<&str, Vec<Option<u8>>>,
        separated_list!(tag!(" "), time_item)
 );
 
-// named!(cron_item<&str, CronItem>,
-//        do_parse!(
-//                minute:          time_item >>
-
-//                hour:            time_item >>
-//                day_of_month:    time_item >>
-//                month:           time_item >>
-//                day_of_week:     time_item >>
-//                (CronItem {
-//                    minute: minute,
-//                    hour: hour,
-//                    day_of_month: day_of_month,
-//                    month: month,
-//                    day_of_week: day_of_week,
-//                    command: String::from("ls")
-//                })
-//        )
-// );
+named!(cron_item<&str, CronItem>,
+       do_parse!(
+               minute:          time_item >>
+               tag!(" ") >>
+               hour:            time_item >>
+               tag!(" ") >>
+               day_of_month:    time_item >>
+               tag!(" ") >>
+               month:           time_item >>
+               tag!(" ") >>
+               day_of_week:     time_item >>
+               tag!(" ") >>
+               (CronItem {
+                   minute: minute,
+                   hour: hour,
+                   day_of_month: day_of_month,
+                   month: month,
+                   day_of_week: day_of_week,
+                   command: String::from("ls")
+               })
+       )
+);
 
 // #[test]
 // fn parse_cron_item() {
@@ -80,4 +85,9 @@ fn parse_empty_time_item() {
 #[test]
 fn parse_list_time_items() {
     assert_eq!(time_items("* 1 2 *  "), Ok(("  ", vec!(None, Some(1), Some(2), None))));
+}
+
+#[test]
+fn parse_cron_item() {
+    assert_eq!(cron_item("* 1 * 2 * "), Ok(("", CronItem { minute: None, hour: Some(1), day_of_month: None, month:  Some(2), day_of_week: None, command: String::from("ls") })));
 }
