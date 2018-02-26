c8cada3cc560f2e0df1ecd20a8f4d8497011d222
Ayrat Badykov <ayratin555@gmail.com>
2018-02-26 11:44:37 +0300

    implement Display trait for CronItem

--- src/cron_item.rs
+++ src/cron_item.rs
@@ -38,13 +38,24 @@ impl Display for TimeItem {
             &TimeItem::Interval((start, end)) => write!(f, "{}-{}", start, end),
             &TimeItem::MultipleValues(ref values) =>  {
                 let result = values.iter()
-                    .map(|val| val.to_string().as_str())
-                    .collect::<Vec<&str>>().join(",");
+                    .map(|val| val.to_string())
+                    .fold(String::from(""), |mut acc, val| {
+                        acc.push_str(&val);
+                        acc.push_str(",");
 
-                write!(f, "{}", result)
+                        acc
+                    });
+
+                write!(f, "{}", &result[0..result.len()-1])
             }
-            &TimeItem::SingleValue(value)     => write!(f, "{}", value)
+            &TimeItem::SingleValue(value) => write!(f, "{}", value)
         }
+    }
+}
+
+impl Display for CronItem {
+    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+        write!(f, "{} {} {} {} {} {}", self.minute, self.hour, self.day_of_month, self.month, self.day_of_week, self.command)
     }
 }
 
@@ -52,6 +63,7 @@ mod test {
     use super::CronItem;
     use super::TimeItem::*;
     use std::str::FromStr;
+    use std::string::ToString;
 
     #[test]
     fn create_cron_item_from_str() {
@@ -67,5 +79,47 @@ mod test {
                        command: String::from("sudo rm -rf /")
                    }
         );
+    }
+
+    #[test]
+    fn convert_time_item_interval_to_string() {
+        let interval = Interval((1, 10));
+
+        assert_eq!("1-10", interval.to_string());
+    }
+
+    #[test]
+    fn convert_time_item_single_value_to_string() {
+        let value = SingleValue(5);
+
+        assert_eq!("5", value.to_string());
+    }
+
+    #[test]
+    fn convert_time_item_multiple_values_to_string() {
+        let value = MultipleValues(vec!(1, 5, 7));
+
+        assert_eq!("1,5,7", value.to_string());
+    }
+
+    #[test]
+    fn convert_time_item_all_values_to_string() {
+        let value = AllValues;
+
+        assert_eq!("*", value.to_string());
+    }
+
+    #[test]
+    fn convert_cron_item_to_string() {
+        let cron_item = CronItem {
+            minute: MultipleValues(vec!(1, 10)),
+            hour:   Interval((1, 4)),
+            day_of_month: Interval((1,11)),
+            month: MultipleValues(vec!(1,2,5)),
+            day_of_week: AllValues,
+            command: String::from("sudo rm -rf /")
+        };
+
+        assert_eq!("1,10 1-4 1-11 1,2,5 * sudo rm -rf /", cron_item.to_string());
     }
 }
